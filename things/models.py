757912ee6bd56693ca828655f4f59fe2376c8a59
Adam Stankiewicz <stan.adamm@gmail.com>
2023-10-08 20:58:11 +0100

    Created Migration for Things

--- things/models.py
+++ things/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.db.models import Model
-from django.core.validators import MaxValueValidator, MinValueValidator
+from django.core.validators import MaxValueValidator
 
 class Thing(Model):
     name = models.CharField(
--- things/tests.py
+++ things/tests.py
@@ -1,24 +1,24 @@
-# from django.test import TestCase
-# from django.forms import ValidationError
-# from .models import Thing
+from django.test import TestCase
+from django.forms import ValidationError
+from .models import Thing
 
-# class thingsModelTest(TestCase):
-#     def setUp(self): # runs before each test
-#         self.thing = Thing.objects.create(name="test", description="", quantity="50")
+class thingsModelTest(TestCase):
+    def setUp(self): # runs before each test
+        self.thing = Thing.objects.create(name="test", description="", quantity="50")
         
-#     def test_integer_thing(self):
-#         self.assert_thing_is_valid()
+    def test_integer_thing(self):
+        self.assert_thing_is_valid()
 
-#     def test_integer_thing_over_100(self):
-#         self.thing.quantity=101
-#         self.assert_thing_is_invalid()
+    def test_integer_thing_over_100(self):
+        self.thing.quantity=101
+        self.assert_thing_is_invalid()
 
-#     def assert_thing_is_valid(self):
-#         try:
-#             self.thing.full_clean()
-#         except (ValidationError):
-#             self.fail("Test thing should be valid")
+    def assert_thing_is_valid(self):
+        try:
+            self.thing.full_clean()
+        except (ValidationError):
+            self.fail("Test thing should be valid")
 
-#     def assert_thing_is_invalid(self):
-#         with self.assertRaises(ValidationError):
-#             self.thing.full_clean()
+    def assert_thing_is_invalid(self):
+        with self.assertRaises(ValidationError):
+            self.thing.full_clean()
